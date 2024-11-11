2d38b4d0b812d8e407049e74e26859ec17535a51
balandina-o <balandina.olga.2021@gmail.com>
2024-11-12 00:32:30 +0500

    further initialization

---
+++ types/src/components/buttons/Button.d.ts
@@ -0,0 +1,6 @@
+import { FC } from "react";
+type ButtonProps = {
+    label: string;
+};
+declare const Button: FC<ButtonProps>;
+export default Button;
--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,25 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: path.resolve(__dirname, "./src/index.ts"),
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd",
+    clean: true /** Очистка папки при каждой сборке */
+  },
+  resolve: {
+    extensions: [ ".ts", ".tsx"],
+  },
+  externals: {
+    react: "react",
+  },
   module: {
     rules: [
       {
-        test: /\.ts?$/,
+        test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
-  },
-  resolve: {
-    extensions: [ ".ts", ".js"],
-  },
-  output: {
-    filename: "script.js",
-    path: path.resolve(__dirname, "public", "static", "bundle"),
   },
 };
